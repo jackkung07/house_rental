e25e0041f75907ef60b5fd9ae3f8dde977489307
ivanybma <ivanybma@ivanybmas-MacBook-Pro.local>
2016-05-10 00:49:02 -0700

    controller coding(ivan)

--- src/main/java/com/houserental/repository/tenant/TenantRepo.java
+++ src/main/java/com/houserental/repository/tenant/TenantRepo.java
@@ -1,8 +1,11 @@
 package com.houserental.repository.tenant;
 
+import com.houserental.entity.landlord.HouseInfo;
 import com.houserental.entity.tenant.Tenant;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * Created by cheyikung on 4/17/16.
@@ -16,4 +19,8 @@ public interface TenantRepo extends CrudRepository<Tenant, String>, TenantRepoCu
 
     @Query("{'name' : ?0}")
     public Tenant findByName(String name);
+
+//    @Query("{'' : ?0}")
+//    public List<HouseInfo> findByFirstNameLike(String value);
+
 }
